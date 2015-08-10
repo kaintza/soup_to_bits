f5600479f283a06c957a7381df4acb4327f62fa3
turkimurki <adam.kaintz@gmail.com>
2015-08-10 22:09:20 +0200

    toggle change with flash

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,7 +1,7 @@
 class SoupsController < ApplicationController
+before_action :fetch_soup, only: [:show, :edit, :update, :destroy, :toggle_featured]
 
   def show
-    @soup = Soup.find(params[:id])
     respond_to do |format|
       format.html
       format.json {render json: @soup}
@@ -14,6 +14,30 @@ class SoupsController < ApplicationController
     respond_to do |format|
       format.json { render json: @soups}
     end
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def toggle_featured
+    @soup.toggle!(:featured)
+    flash[:notice] = "succesfully changed"
+    redirect_to @soup
+  end
+
+  private
+
+  def fetch_soup
+    @soup = Soup.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :categories, :soups
+  root to: "categories#index"
+  get "/allcategories", to: redirect("/")
+
+  get "/soups/:id/toggle_featured", to: "soups#toggle_featured", as: :toggle_featured
 end
